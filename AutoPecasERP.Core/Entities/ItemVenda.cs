f31412c47fe199692c090eb019a1cfcdf1d85f8c
clickpecasdigital-sistemaclick <clickpecasdigital@gmail.com>
2026-09-24 16:40:12 -0300

    Alinha contratos de domínio com serviços

--- AutoPecasERP.Core/Entities/ItemVenda.cs
+++ AutoPecasERP.Core/Entities/ItemVenda.cs
@@ -1,7 +1,1 @@
-namespace AutoPecasERP.Core.Entities;
-public class ItemVenda {
- public int Id {get;set;} public int VendaId {get;set;} public Venda? Venda {get;set;}
- public int ProdutoId {get;set;} public Produto? Produto {get;set;} public string Descricao {get;set;}="";
- public decimal Quantidade {get;set;} public decimal ValorUnitario {get;set;} public decimal Desconto {get;set;}
- public decimal Total {get;set;}
-}
+namespace AutoPecasERP.Core.Entities; public class ItemVenda{public int Id{get;set;}public int VendaId{get;set;}public Venda? Venda{get;set;}public int ProdutoId{get;set;}public Produto? Produto{get;set;}public string Descricao{get;set;}="" ;public decimal Quantidade{get;set;}public decimal ValorUnitario{get;set;}public decimal PrecoUnitario{get=>ValorUnitario;set=>ValorUnitario=value;}public decimal Desconto{get;set;}public decimal Total{get;set;}}
