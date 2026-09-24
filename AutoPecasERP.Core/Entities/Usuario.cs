a1c71edca10b9e669e8f6e6ebe9e694561e6e6d2
clickpecasdigital-sistemaclick <clickpecasdigital@gmail.com>
2026-09-24 16:38:09 -0300

    Alinha usuário com autenticação

--- AutoPecasERP.Core/Entities/Usuario.cs
+++ AutoPecasERP.Core/Entities/Usuario.cs
@@ -1,3 +1,1 @@
-namespace AutoPecasERP.Core.Entities;
-public enum PerfilUsuario{Admin,Gerente,Vendedor,Estoquista,Financeiro}
-public class Usuario{public int Id{get;set;}public string Login{get;set;}="";public string Nome{get;set;}="";public string SenhaHash{get;set;}="";public PerfilUsuario Perfil{get;set;}=PerfilUsuario.Vendedor;public bool Ativo{get;set;}=true;}
+using AutoPecasERP.Core.Enums; namespace AutoPecasERP.Core.Entities; public class Usuario{public int Id{get;set;}public string Login{get;set;}="" ;public string Nome{get;set;}="" ;public string SenhaHash{get;set;}="" ;public PerfilUsuario Perfil{get;set;}=PerfilUsuario.Vendedor;public bool Ativo{get;set;}=true;public int TentativasFalhas{get;set;}public DateTime? BloqueadoAte{get;set;}}
