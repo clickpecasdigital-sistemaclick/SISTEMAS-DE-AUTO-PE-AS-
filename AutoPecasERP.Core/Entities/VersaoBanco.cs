b038ac016221a7e7161bc61f15aadc31feb11915
clickpecasdigital-sistemaclick <clickpecasdigital@gmail.com>
2026-09-24 16:34:33 -0300

    Restaura entidades centrais ausentes

---
+++ AutoPecasERP.Core/Entities/VersaoBanco.cs
@@ -0,0 +1,2 @@
+namespace AutoPecasERP.Core.Entities;
+public class VersaoBanco{public int Id{get;set;}public int Versao{get;set;}public DateTime AplicadaEm{get;set;}=DateTime.Now;public string? Descricao{get;set;}}
