c707d3134bf18fcae0f90cd35618830ed2cfeacc
clickpecasdigital-sistemaclick <clickpecasdigital@gmail.com>
2026-09-24 16:34:35 -0300

    Restaura entidades centrais ausentes

---
+++ AutoPecasERP.Core/Entities/TabelaPreco.cs
@@ -0,0 +1,2 @@
+namespace AutoPecasERP.Core.Entities;
+public class TabelaPreco{public int Id{get;set;}public string Nome{get;set;}="";public decimal PercentualAjuste{get;set;}public bool Ativa{get;set;}=true;}
